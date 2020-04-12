a4a1b78f576c84296a86e028f1f3a30eb2834f21
K N V ASHOK <meeturfrndashok8@gmail.com>
2020-04-12 12:59:59 +0530

    Update Solution.cpp

--- Week 1: April 1st–April 7th/Single Number/Solution.cpp
+++ Week 1: April 1st–April 7th/Single Number/Solution.cpp
@@ -1,1 +1,10 @@
-sd
+class Solution {
+public:
+    int singleNumber(vector<int>& a) {
+        int n = 0;    
+        for(int i : a){
+            n ^= i;
+        }    
+        return n;
+    }
+};
